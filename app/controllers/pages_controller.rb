ecf3623a2e426c780caa22b3333923103c069fba
Marcel Fonseca <marcelsf23br@gmail.com>
2023-11-29 10:53:35 +0100

    Add basic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query]
+      @albums = RSpotify::Album.search(params[:query])
+    end
   end
 end
